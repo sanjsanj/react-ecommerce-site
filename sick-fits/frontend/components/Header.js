1345f3507628b8cebbcdd5bc51bbd6e99ed3b44a
Sanjay Purswani <sanjsanj@hotmail.com>
2018-10-29 20:05:24 +0000

    Use next/router and nprogress to visualise route change

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,19 @@
-import Link from "next/link";
 import styled from "styled-components";
+import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import Nav from "./Nav";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
