7adf04059e2748ba7edbaf21ceb2521645538912
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-04 10:58:25 +0000

    Setup apollo client with react SSR

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,19 +1,34 @@
 import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
 
+import withData from "../lib/withData";
 import Page from "../components/Page";
 
-export default class MyApp extends App {
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <div>
         <Container>
-          <Page>
-            <Component />
-          </Page>
+          <ApolloProvider client={apollo}>
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </ApolloProvider>
         </Container>
       </div>
     );
   }
 }
+
+export default withData(MyApp);
