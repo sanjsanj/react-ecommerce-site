0311fc6adac2f3fe8fd7e410953ca9c4338c0e56
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-05 19:29:22 +0000

    Item is connected to a User

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,11 +7,16 @@ const { transport, emailTemplate } = require("../mail");
 
 const Mutation = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    if (!ctx.request.userId) throw new Error("You must be logged in to do that");
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args
         }
       },
