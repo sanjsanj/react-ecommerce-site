e54c6528edea136dcabab8128223dfb6c116eb97
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-05 17:38:49 +0000

    Backend can request reset and reset password

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 
 const Mutation = {
   async createItem(parent, args, ctx, info) {
@@ -93,6 +95,61 @@ const Mutation = {
     ctx.response.clearCookie("token");
 
     return { message: "Signed out" };
+  },
+
+  async requestReset(parent, { email }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+
+    if (!user) throw new Error(`No user found for email ${email}`);
+
+    const randomBytesPromise = await promisify(randomBytes)(20);
+    const resetToken = randomBytesPromise.toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000;
+
+    await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry }
+    });
+
+    return { message: "Password reset email sent" };
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    const { resetToken, password, confirmPassword } = args;
+
+    if (password !== confirmPassword) throw new Error("Passwords do not match");
+
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+
+    if (!user) throw new Error("This token is either invalid or expired");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const updatedUser = await ctx.db.mutation.updateUser(
+      {
+        where: { email: user.email },
+        data: {
+          password: hashedPassword,
+          resetToken: null,
+          resetTokenExpiry: null
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return updatedUser;
   }
 };
 
