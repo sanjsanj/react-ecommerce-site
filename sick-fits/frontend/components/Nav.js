3068f5c487e84244a56457d1175113d1c21339bd
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-06 18:44:06 +0000

    Can toggle cart open and closed with local client state mutations and queries

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { Mutation } from "react-apollo";
 
 import NavStyles from "./styles/NavStyles";
 
 import User from "./User";
 import Signout from "./Signout";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const Nav = () => (
   <User>
@@ -28,6 +30,10 @@ const Nav = () => (
             </Link>
 
             <Signout />
+
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </>
         )}
 
