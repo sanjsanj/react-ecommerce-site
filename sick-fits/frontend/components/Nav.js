2213d00a51eeab2bb12b8a6b8f89aa2d781c0992
Sanjay Purswani <sanjsanj@hotmail.com>
2018-11-07 18:20:21 +0000

    Nav has animated cart count badge

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -6,6 +6,7 @@ import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
 import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 
 const Nav = () => (
   <User>
@@ -32,7 +33,17 @@ const Nav = () => (
             <Signout />
 
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (acc, CartItem) => acc + CartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
